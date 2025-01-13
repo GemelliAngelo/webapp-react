c20e23d534f4920df76cfba5394b4feefdf3050d
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-13 14:47:25 +0100

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import Movies from "./pages/Movies";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
       </Routes>
     </BrowserRouter>
   );
