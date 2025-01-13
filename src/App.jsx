8a85e8ee95e33cf6f4e07ed2777995f8fe643d35
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-13 14:24:51 +0100

    add bootstrap cs and js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
 import "./assets/css/index.css";
 
 function App() {
   return (
     <>
-      <h1>Connesso</h1>
+      <h1>React Cinema</h1>
     </>
   );
 }
